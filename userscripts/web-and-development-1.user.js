b3193df1f0a62f3a844b0c8f87ae3b01ba8c56e1
Onur Sahin <44167666+Onur169@users.noreply.github.com>
2020-12-12 14:44:55 +0100

    Tampermonkey Boilerplate

--- userscripts/web-and-development-1.user.js
+++ userscripts/web-and-development-1.user.js
@@ -1,1 +1,16 @@
-console.log("Hallo Welt.");
+// ==UserScript==
+// @name         klima-streik.org/
+// @namespace    https://www.klima-streik.org/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.klima-streik.org/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log("Das ist ein Test!");
+
+})();
