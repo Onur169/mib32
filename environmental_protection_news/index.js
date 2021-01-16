e8573af427b5294700fd7d4194aea52ffcad1a61
Dustin Bastke <dustin.bastke@online.de>
2021-01-16 18:56:19 +0100

    v0.3

--- environmental_protection_news/index.js
+++ environmental_protection_news/index.js
@@ -5,18 +5,22 @@ let cheerio = require ("cheerio");
 let crawler = new Crawler('https://www.nabu.de/umwelt-und-ressourcen/index.html');
 crawler.maxDepth = 2;
 
+// check Connection
 crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
     console.log(queueItem.url);
     console.log(response.statusCode);
 });
 
+// crawl through LI Elements > DIV Elements
 crawler.discoverResources = function(buffer, queueItem) {
     var $ = cheerio.load(buffer.toString("utf8"));
 
-    return $("li[class]").map(function () {
-        console.log($(this).attr('class'));
+    return $("li[class] > div").map(function () {
+        crawler.filter('.image').first();
+        console.log($(this).filter("class"));
         return $(this).attr("class");
     }).get();
+
 };
 
 crawler.start();
