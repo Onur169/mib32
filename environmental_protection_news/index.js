2fc4b8f5c917f46326e5e95b00c13e078d8f34b3
Onur Sahin <44167666+Onur169@users.noreply.github.com>
2021-01-16 17:14:53 +0100

    package.json

--- environmental_protection_news/index.js
+++ environmental_protection_news/index.js
@@ -1,37 +1,11 @@
 let Crawler = require("simplecrawler");
-let cheerio = require ("cheerio");
+let cheerio = require("cheerio");
+
 // initiate Crawler
 let crawler = new Crawler('https://www.nabu.de/umwelt-und-ressourcen/index.html');
-// crawler.maxDepth = 2;
+
 crawler.maxConcurrency = 2;
 
-
-        /*
-        {
-
-            image: "bla image",
-            headline: "bla headline",
-            preview_text: "bla preview text",
-            external_page: "bla exteral page"
-
-        }
-        */
-
-    /*
-crawler.discoverResources = function(buffer, queueItem) {
-
-    var $ = cheerio.load(buffer.toString("utf8"));
-
-    return $(".column .image .slide").map(function () {
-
-
-        return $(this).attr("href");
-        
-    }).get();
-
-
-};
-*/
 
 crawler.discoverResources = function(buffer, queueItem) {
 
@@ -39,7 +13,7 @@ crawler.discoverResources = function(buffer, queueItem) {
 
     var $ = cheerio.load(buffer.toString("utf8"));
 
-    console.log($(".slide"));
+    console.log($, "eee");
 
     //console.log(news);
     
@@ -49,14 +23,10 @@ crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
 
 
 
-    //console.log("I just received %s (%d bytes)", queueItem.url, responseBuffer.length);
+    console.log("I just received %s (%d bytes)", queueItem.url, responseBuffer.length);
     //console.log("It was a resource of type %s", response.headers['content-type']);
 
 });
 
 
 crawler.start();
-
-
-
-
