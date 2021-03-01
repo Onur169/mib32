435816a5644d11aeb9472ae7a83e448141e0fb5b
Christian <Christian.Knoth@iem.thm.de>
2021-03-01 15:42:36 +0100

    created timer-component

--- project/spa/src/app/app.module.ts
+++ project/spa/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './header/navigation/navigation.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    TimerComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
