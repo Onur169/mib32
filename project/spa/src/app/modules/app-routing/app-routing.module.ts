edcd0d961678d10a28e3d289c4c76c1b03d22501
Christian <Christian.Knoth@iem.thm.de>
2021-03-02 14:53:10 +0100

    added meta

--- project/spa/src/app/modules/app-routing/app-routing.module.ts
+++ project/spa/src/app/modules/app-routing/app-routing.module.ts
@@ -1,5 +1,13 @@
+/**
+ * @param createdBy
+ * Christian Knoth.
+ * @param author
+ * Christian Knoth.
+ * @param summary
+ * Organisiert den Router-Outlet um weitere Optionen als Content neben der landing-Page offen zu halten.
+ */
+
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { Routes, RouterModule } from '@angular/router';
 import { LandingpageComponent } from 'src/app/components/landingpage/landingpage.component';
