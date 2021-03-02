591909ae7109490f1e5bf40e9e70aa4a17fec32d
Christian <Christian.Knoth@iem.thm.de>
2021-03-02 18:50:00 +0100

    added httpClient to module

--- project/spa/src/app/app.module.ts
+++ project/spa/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './modules/app-routing/app-routing.module';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     NgbModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
