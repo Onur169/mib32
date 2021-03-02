de41472aaf18f15d28e7b2c4de5eacf7ebd36aaa
Christian <Christian.Knoth@iem.thm.de>
2021-03-02 14:55:09 +0100

    created interface for map and timer

---
+++ project/spa/src/app/helpers/interfaces/Demonstration.ts
@@ -0,0 +1,22 @@
+/**
+ * @param createdBy
+ * Christian Knoth.
+ * @param authors
+ * Christian Knoth.
+ * @param summary
+ * Das Demonsration-Inferface gibt die Attribute eines Events vor und wird im Rahmen der Timer- und der Event-Komponente genutzt.
+ * Ebenso für den Typ-sicheren Empfang eines Events.
+ *
+ */
+
+export interface Demonstration {
+    id: string;
+    name: string;
+    description: string;
+    start_at: Date;
+    end_at: Date;
+    lat: number;
+    lng: number;
+    created_at: Date;
+    deleted_at: Date;
+}
