06d15fed1721fa78e002e65d6b0630638f0380de
Dustin Bastke <dustin.bastke@online.de>
2021-03-04 16:11:36 +0100

    add Comment, separator v1.0 co Author Stefanie Roddeck

--- project/spa/src/app/components/separators/first/first.component.ts
+++ project/spa/src/app/components/separators/first/first.component.ts
@@ -1,3 +1,12 @@
+/**
+ * @param createdBy
+ * Dustin Bastke
+ * @param authors
+ * Stefanie Roddeck, Dustin Bastke
+ * @param summary
+ * Die First-Komponente stellt den ersten Trenner zwischen dem Timer und der Map dar.
+ */
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
