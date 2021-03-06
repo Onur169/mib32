8aa5f4f627e560201f1ddecf3b9d0516e895ad50
Christian <Christian.Knoth@iem.thm.de>
2021-03-06 15:50:47 +0100

    adapted convention

--- project/spa/src/app/helpers/classes/ThrowbackManager.ts
+++ project/spa/src/app/helpers/classes/ThrowbackManager.ts
@@ -12,27 +12,27 @@ import { ThrowbackClass } from "./ThrowbackClass";
  export class ThrowbackManager{
 
    public pages: Map<number, ThrowbackClass[]>=new Map<number, ThrowbackClass[]>();
-   private current_page: number;
+   private currentPage: number;
    private max_pages: number;
 
    constructor(){
 
-     this.current_page=1;
+     this.currentPage=1;
      this.max_pages=1;
    }
 
      /**
    * Wenn via http-request eine neue Seite geladen wird, so soll sie hiermit gesetzt werden.
-   * @param new_page -Seitenzahl
+   * @param newPage -Seitenzahl
    * @param markers -Events
    * **/
-  setnewPage(new_page:number, markers: ThrowbackClass[]): void{
+  setnewPage(newPage:number, markers: ThrowbackClass[]): void{
 
-    this.pages.set(new_page,  markers);
+    this.pages.set(newPage,  markers);
    }
 
    /**
-   * Gibt das erste Event der ersten Deite aus (hauptsächlich für den Timer).
+   * Gibt das erste Throwback der ersten Seite aus.
    * **/
   getFirstThrowback(): ThrowbackClass | undefined{
     if(this.pages.get(1))return this.pages.get(1)![0];
@@ -43,17 +43,17 @@ import { ThrowbackClass } from "./ThrowbackClass";
   }
 
      //für das Überschreiben
-  setMaxPages(max_pages: number): void{
+  setMaxPages(max_pages: number): void{//Onur rausnehmen
     this.max_pages=max_pages;
   }
 
-  setCurrentPage(current_page: number): void{
-    this.current_page=current_page;
+  setCurrentPage(currentPage: number): void{
+    this.currentPage=currentPage;
   }
 
     //sollte nur für die allererste Abfrage genutzt werden
     getCurrentPage(): number{
-      return this.current_page;
+      return this.currentPage;
     }
 
 
