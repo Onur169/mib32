28c06383fc5b50fcf9fcc95d90aaae411ae86290
Christian <Christian.Knoth@iem.thm.de>
2021-03-15 22:06:19 +0100

    added filler three+ animations

--- project/spa/src/app/components/filler/fillerthree/fillerthree.component.ts
+++ project/spa/src/app/components/filler/fillerthree/fillerthree.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 @Component({
   selector: 'app-fillerthree',
   templateUrl: './fillerthree.component.html',
@@ -10,6 +12,40 @@ export class FillerthreeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  ngAfterViewInit(){
+    this.scrollUp();
+  }
+
+
+  scrollUp(){
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    var t1=gsap.from("#fillerthree_bg",{
+      scrollTrigger: {
+        trigger:"#fillerthree_bg",
+        start:"top 90%",
+        end:"center 60%",
+        scrub: true,
+        markers: false,
+        toggleActions:"restart pause reverse pause" //wenn sichtbar, wenn nicht sichtbar, wenn wieder zurück
+      },
+      opacity:0
+    });
+
+    var t2=gsap.from("#fillerthree_box",{
+      scrollTrigger: {
+        trigger:"#fillerthree_box",
+        start:"top 75%",
+            end:"top 50%",
+            scrub: true,
+            markers: false,
+            toggleActions:"restart pause reverse pause" //wenn sichtbar, wenn nicht sichtbar, wenn wieder zurück
+          },
+          x: -500
+        });
+  }
 }
